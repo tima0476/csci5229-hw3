80abebaf89704bde7519f4c955185a2cd99292e8
Tim Mason <tima0476@colorado.edu>
2019-09-24 23:19:44 -0600

    Got the rocket tubes working (finless).

---
+++ hw3.c
@@ -0,0 +1,743 @@
+/*
+ *  Timothy Mason, homework 3, Scene in 3D
+ * CSCI 5229 Computer Graphics - University of Colorado Boulder
+ *
+ * Based heavily on example 8, ex8.c
+ *
+ *  Key bindings:
+ *  a          Toggle axes
+ *  arrows     Change view angle
+ *  0          Reset view angle
+ *  ESC        Exit
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <math.h>
+
+#include "rocket.h"
+
+//  OpenGL with prototypes for glext
+#define GL_GLEXT_PROTOTYPES
+#ifdef __APPLE__
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+int th=0;         //  Azimuth of view angle
+int ph=0;         //  Elevation of view angle
+double zh=0;      //  Rotation of teapot
+int axes=1;       //  Display axes
+int mode=0;       //  What to display
+
+//  Cosine and Sine in degrees
+#define Cos(x) (cos((x)*M_PI/180))
+#define Sin(x) (sin((x)*M_PI/180))
+
+/*
+ *  Convenience routine to output raster text
+ *  Use VARARGS to make this more flexible
+ */
+#define LEN 256  //  Maximum length of text string
+void Print(const char* format , ...)
+{
+   char    buf[LEN];
+   char*   ch=buf;
+   va_list args;
+   //  Turn the parameters into a character string
+   va_start(args,format);
+   vsnprintf(buf,LEN,format,args);
+   va_end(args);
+   //  Display the characters one at a time at the current raster position
+   while (*ch)
+      glutBitmapCharacter(GLUT_BITMAP_HELVETICA_18,*ch++);
+}
+
+/*
+ *  Draw a cube
+ *     at (x,y,z)
+ *     dimensions (dx,dy,dz)
+ *     rotated th about the y axis
+ */
+static void cube(double x,double y,double z,
+                 double dx,double dy,double dz,
+                 double th)
+{
+   //  Save transformation
+   glPushMatrix();
+   //  Offset
+   glTranslated(x,y,z);
+   glRotated(th,0,1,0);
+   glScaled(dx,dy,dz);
+   //  Cube
+   glBegin(GL_QUADS);
+   //  Front
+   glColor3f(1,0,0);
+   glVertex3f(-1,-1, 1);
+   glVertex3f(+1,-1, 1);
+   glVertex3f(+1,+1, 1);
+   glVertex3f(-1,+1, 1);
+   //  Back
+   glColor3f(0,0,1);
+   glVertex3f(+1,-1,-1);
+   glVertex3f(-1,-1,-1);
+   glVertex3f(-1,+1,-1);
+   glVertex3f(+1,+1,-1);
+   //  Right
+   glColor3f(1,1,0);
+   glVertex3f(+1,-1,+1);
+   glVertex3f(+1,-1,-1);
+   glVertex3f(+1,+1,-1);
+   glVertex3f(+1,+1,+1);
+   //  Left
+   glColor3f(0,1,0);
+   glVertex3f(-1,-1,-1);
+   glVertex3f(-1,-1,+1);
+   glVertex3f(-1,+1,+1);
+   glVertex3f(-1,+1,-1);
+   //  Top
+   glColor3f(0,1,1);
+   glVertex3f(-1,+1,+1);
+   glVertex3f(+1,+1,+1);
+   glVertex3f(+1,+1,-1);
+   glVertex3f(-1,+1,-1);
+   //  Bottom
+   glColor3f(1,0,1);
+   glVertex3f(-1,-1,-1);
+   glVertex3f(+1,-1,-1);
+   glVertex3f(+1,-1,+1);
+   glVertex3f(-1,-1,+1);
+   //  End
+   glEnd();
+   //  Undo transformations
+   glPopMatrix();
+}
+
+/*
+ * Draw vertex in cylindrical coordinates (r, theta, z)
+ */
+static void cylVertex(double r, double th, double z)
+{
+   glVertex3d(r*Cos(th), r*Sin(th), z);
+}
+
+/*
+ * Draw a radially symmetric solid
+ *
+ * profile: pointer to an array of x,y coordinates representing the surface profile of the solid
+ * size: The number of points in the profile array
+ * bz,by,bz: 3D coordinates of the base of the solid
+ * rx,ry,rz: 3D vector for rotation of the solid.
+ * ph:  Angle to rotate the solid around (rx,ry,rz)
+ * s: the scale of the solid
+ */
+static void lathe(dpp profile, int size, double bx, double by,double bz, double rx, double ry, double rz, double ph, double s)
+{
+   const int d=15;
+   int th,i;
+
+   // Save transformation
+   glPushMatrix();
+
+   // Offset and scale
+   glTranslated(bx,by,bz);
+   glRotated(ph, rx, ry, rz);
+   glScaled(s,s,s);
+
+   // Latitude bands
+   for (i=1; i<size; i++)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (th=0; th<=360; th+=d)
+      {
+         cylVertex(profile[i-1].x, th, profile[i-1].y);
+         cylVertex(profile[i].x, th, profile[i].y);
+      }
+      glEnd();
+   }
+
+   // Top cap; if necessary
+   if (profile[0].x != 0.0)
+   {
+      glBegin(GL_TRIANGLE_FAN);
+      glColor3f(0,0,1);
+      glVertex3d(0, profile[0].y, 0);
+      for (th = 0; th <= 360; th += d)
+         cylVertex(profile[0].x, th, profile[0].y);
+      glEnd();
+   }
+
+   // Bottom cap; if necessary
+   if (profile[size-1].x != 0.0)
+   {
+      // Draw a triangle fan from the origin to the final circle.
+      glBegin(GL_TRIANGLE_FAN);
+      glColor3f(0,0,1);
+      glVertex3d(0, profile[size-1].y, 0);
+      for (th = 0; th <= 360; th += d)
+         cylVertex(profile[size-1].x, th, profile[size-1].y);
+      glEnd();
+   }
+
+   // undo transformations
+   glPopMatrix();
+}
+
+/*
+ *  Draw vertex in spherical coordinates
+ */
+static void sphVertex(double th,double ph)
+{
+   glColor3f(Cos(th)*Cos(th) , Sin(ph)*Sin(ph) , Sin(th)*Sin(th));
+   glVertex3d(Sin(th)*Cos(ph) , Sin(ph) , Cos(th)*Cos(ph));
+}
+
+/*
+ *  Draw a sphere (version 1)
+ *     at (x,y,z)
+ *     radius (r)
+ */
+static void sphere1(double x,double y,double z,double r)
+{
+   const int d=15;
+   int th,ph;
+
+   //  Save transformation
+   glPushMatrix();
+   //  Offset and scale
+   glTranslated(x,y,z);
+   glScaled(r,r,r);
+
+   //  South pole cap
+   glBegin(GL_TRIANGLE_FAN);
+   sphVertex(0,-90);
+   for (th=0;th<=360;th+=d)
+   {
+      sphVertex(th,d-90);
+   }
+   glEnd();
+
+   //  Latitude bands
+   for (ph=d-90;ph<=90-2*d;ph+=d)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (th=0;th<=360;th+=d)
+      {
+         sphVertex(th,ph);
+         sphVertex(th,ph+d);
+      }
+      glEnd();
+   }
+
+   //  North pole cap
+   glBegin(GL_TRIANGLE_FAN);
+   sphVertex(0,90);
+   for (th=0;th<=360;th+=d)
+   {
+      sphVertex(th,90-d);
+   }
+   glEnd();
+
+   //  Undo transformations
+   glPopMatrix();
+}
+
+/*
+ *  Draw a sphere (version 2)
+ *     at (x,y,z)
+ *     radius (r)
+ */
+static void sphere2(double x,double y,double z,double r)
+{
+   const int d=5;
+   int th,ph;
+
+   //  Save transformation
+   glPushMatrix();
+   //  Offset and scale
+   glTranslated(x,y,z);
+   glScaled(r,r,r);
+
+   //  Latitude bands
+   for (ph=-90;ph<90;ph+=d)
+   {
+      glBegin(GL_QUAD_STRIP);
+      for (th=0;th<=360;th+=d)
+      {
+         sphVertex(th,ph);
+         sphVertex(th,ph+d);
+      }
+      glEnd();
+   }
+
+   //  Undo transformations
+   glPopMatrix();
+}
+
+/*
+ *  Draw a airplane shaped polygon at (x,y,z)
+ */
+static void PolyPlane(int type,double x,double y,double z)
+{
+   //  Save transformation
+   glPushMatrix();
+   //  Offset
+   glTranslated(x,y,z);
+   //  Fuselage and wings
+   glColor3f(1,1,0); 
+   glBegin(type);
+   glVertex2f( 1.0, 0.0);
+   glVertex2f( 0.8, 0.1);
+   glVertex2f( 0.0, 0.1);
+   glVertex2f(-1.0, 0.5);
+   glVertex2f(-1.0,-0.5);
+   glVertex2f( 0.0,-0.1);
+   glVertex2f( 0.8,-0.1);
+   glEnd();
+   //  Vertical tail
+   glColor3f(1,0,0);
+   glBegin(type);
+   glVertex3f(-1.0, 0.0,0.0);
+   glVertex3f(-1.0, 0.0,0.5);
+   glVertex3f(-0.5, 0.0,0.0);
+   glEnd();
+   //  Undo transformations
+   glPopMatrix();
+}
+
+/*
+ *  Draw a flat airplane at (x,y,z)
+ */
+static void FlatPlane(double x,double y,double z)
+{
+   //  Save transformation
+   glPushMatrix();
+   //  Offset
+   glRotated(-90,1,0,0);
+   glTranslated(x,y,z);
+   //  Fuselage
+   glColor3f(0,0,1);
+   glBegin(GL_POLYGON);
+   glVertex2f( 1.0, 0.0);
+   glVertex2f( 0.8, 0.1);
+   glVertex2f(-1.0, 0.1);
+   glVertex2f(-1.0,-0.1);
+   glVertex2f( 0.8,-0.1);
+   glEnd();
+   //  Wings
+   glColor3f(1,1,0);
+   glBegin(GL_TRIANGLES);
+   //  Starboard
+   glVertex2f( 0.0, 0.1);
+   glVertex2f(-1.0, 0.1);
+   glVertex2f(-1.0, 0.5);
+   //  Port
+   glVertex2f( 0.0,-0.1);
+   glVertex2f(-1.0,-0.1);
+   glVertex2f(-1.0,-0.5);
+   glEnd();
+   //  Vertical tail
+   glColor3f(1,0,0);
+   glBegin(GL_TRIANGLES);
+   glVertex3f(-1.0, 0.0,0.0);
+   glVertex3f(-1.0, 0.0,0.5);
+   glVertex3f(-0.5, 0.0,0.0);
+   glEnd();
+   //  Undo transformations
+   glPopMatrix();
+}
+
+/*
+ *  Draw solid airplane
+ *    at (x,y,z)
+ *    nose towards (dx,dy,dz)
+ *    up towards (ux,uy,uz)
+ */
+static void SolidPlane(double x,double y,double z,
+                       double dx,double dy,double dz,
+                       double ux,double uy, double uz)
+{
+   // Dimensions used to size airplane
+   const double wid=0.05;
+   const double nose=+0.50;
+   const double cone= 0.20;
+   const double wing= 0.00;
+   const double strk=-0.20;
+   const double tail=-0.50;
+   //  Unit vector in direction of flght
+   double D0 = sqrt(dx*dx+dy*dy+dz*dz);
+   double X0 = dx/D0;
+   double Y0 = dy/D0;
+   double Z0 = dz/D0;
+   //  Unit vector in "up" direction
+   double D1 = sqrt(ux*ux+uy*uy+uz*uz);
+   double X1 = ux/D1;
+   double Y1 = uy/D1;
+   double Z1 = uz/D1;
+   //  Cross product gives the third vector
+   double X2 = Y0*Z1-Y1*Z0;
+   double Y2 = Z0*X1-Z1*X0;
+   double Z2 = X0*Y1-X1*Y0;
+   //  Rotation matrix
+   double mat[16];
+   mat[0] = X0;   mat[4] = X1;   mat[ 8] = X2;   mat[12] = 0;
+   mat[1] = Y0;   mat[5] = Y1;   mat[ 9] = Y2;   mat[13] = 0;
+   mat[2] = Z0;   mat[6] = Z1;   mat[10] = Z2;   mat[14] = 0;
+   mat[3] =  0;   mat[7] =  0;   mat[11] =  0;   mat[15] = 1;
+
+   //  Save current transforms
+   glPushMatrix();
+   //  Offset, scale and rotate
+   glTranslated(x,y,z);
+   glMultMatrixd(mat);
+   //  Nose (4 sided)
+   glColor3f(0,0,1);
+   glBegin(GL_TRIANGLES);
+   glVertex3d(nose, 0.0, 0.0);
+   glVertex3d(cone, wid, wid);
+   glVertex3d(cone,-wid, wid);
+
+   glVertex3d(nose, 0.0, 0.0);
+   glVertex3d(cone, wid,-wid);
+   glVertex3d(cone,-wid,-wid);
+
+   glVertex3d(nose, 0.0, 0.0);
+   glVertex3d(cone, wid, wid);
+   glVertex3d(cone, wid,-wid);
+
+   glVertex3d(nose, 0.0, 0.0);
+   glVertex3d(cone,-wid, wid);
+   glVertex3d(cone,-wid,-wid);
+   glEnd();
+   //  Fuselage (square tube)
+   glBegin(GL_QUADS);
+   glVertex3d(cone, wid, wid);
+   glVertex3d(cone,-wid, wid);
+   glVertex3d(tail,-wid, wid);
+   glVertex3d(tail, wid, wid);
+
+   glVertex3d(cone, wid,-wid);
+   glVertex3d(cone,-wid,-wid);
+   glVertex3d(tail,-wid,-wid);
+   glVertex3d(tail, wid,-wid);
+
+   glVertex3d(cone, wid, wid);
+   glVertex3d(cone, wid,-wid);
+   glVertex3d(tail, wid,-wid);
+   glVertex3d(tail, wid, wid);
+
+   glVertex3d(cone,-wid, wid);
+   glVertex3d(cone,-wid,-wid);
+   glVertex3d(tail,-wid,-wid);
+   glVertex3d(tail,-wid, wid);
+
+   glVertex3d(tail,-wid, wid);
+   glVertex3d(tail, wid, wid);
+   glVertex3d(tail, wid,-wid);
+   glVertex3d(tail,-wid,-wid);
+   glEnd();
+   //  Wings (plane triangles)
+   glColor3f(1,1,0);
+   glBegin(GL_TRIANGLES);
+   glVertex3d(wing, 0.0, wid);
+   glVertex3d(tail, 0.0, wid);
+   glVertex3d(tail, 0.0, 0.5);
+
+   glVertex3d(wing, 0.0,-wid);
+   glVertex3d(tail, 0.0,-wid);
+   glVertex3d(tail, 0.0,-0.5);
+   glEnd();
+   //  Vertical tail (plane triangle)
+   glColor3f(1,0,0);
+   glBegin(GL_POLYGON);
+   glVertex3d(strk, 0.0, 0.0);
+   glVertex3d(tail, 0.3, 0.0);
+   glVertex3d(tail, 0.0, 0.0);
+   glEnd();
+   //  Undo transformations
+   glPopMatrix();
+}
+
+
+/*
+ *  Draw icosahedron
+ *     size  s
+ */
+static void icosahedron(float s)
+{
+   //  sphVertex index list
+   const int N=60;
+   const unsigned char index[] =
+      {
+       2, 1, 0,    3, 2, 0,    4, 3, 0,    5, 4, 0,    1, 5, 0,
+      11, 6, 7,   11, 7, 8,   11, 8, 9,   11, 9,10,   11,10, 6,
+       1, 2, 6,    2, 3, 7,    3, 4, 8,    4, 5, 9,    5, 1,10,
+       2, 7, 6,    3, 8, 7,    4, 9, 8,    5,10, 9,    1, 6,10,
+      };
+   //  sphVertex coordinates
+   const float xyz[] =
+      {
+       0.000, 0.000, 1.000,
+       0.894, 0.000, 0.447,
+       0.276, 0.851, 0.447,
+      -0.724, 0.526, 0.447,
+      -0.724,-0.526, 0.447,
+       0.276,-0.851, 0.447,
+       0.724, 0.526,-0.447,
+      -0.276, 0.851,-0.447,
+      -0.894, 0.000,-0.447,
+      -0.276,-0.851,-0.447,
+       0.724,-0.526,-0.447,
+       0.000, 0.000,-1.000
+      };
+   //  sphVertex colors
+   const float rgb[] =
+      {
+      0.0,0.0,1.0,
+      0.0,1.0,0.0,
+      0.0,1.0,1.0,
+      1.0,0.0,0.0,
+      1.0,0.0,1.0,
+      1.0,1.0,0.0,
+      0.0,0.0,1.0,
+      0.0,1.0,0.0,
+      0.0,1.0,1.0,
+      1.0,0.0,0.0,
+      1.0,0.0,1.0,
+      1.0,1.0,0.0,
+      };
+   //  Define vertexes
+   glVertexPointer(3,GL_FLOAT,0,xyz);
+   glEnableClientState(GL_VERTEX_ARRAY);
+   //  Define colors for each vertex
+   glColorPointer(3,GL_FLOAT,0,rgb);
+   glEnableClientState(GL_COLOR_ARRAY);
+   //  Draw icosahedron
+   glPushMatrix();
+   glScalef(s,s,s);
+   glDrawElements(GL_TRIANGLES,N,GL_UNSIGNED_BYTE,index);
+   glPopMatrix();
+   //  Disable vertex array
+   glDisableClientState(GL_VERTEX_ARRAY);
+   //  Disable color array
+   glDisableClientState(GL_COLOR_ARRAY);
+}
+
+/*
+ *  OpenGL (GLUT) calls this routine to display the scene
+ */
+void display()
+{
+   const double len=1.5;  //  Length of axes
+   //  Erase the window and the depth buffer
+   glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
+   //  Enable Z-buffering in OpenGL
+   glEnable(GL_DEPTH_TEST);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Set view angle
+   glRotatef(ph,1,0,0);
+   glRotatef(th,0,1,0);
+   //  Decide what to draw
+   switch (mode)
+   {
+      //  Draw rocket
+      case 0:  
+         // cube(0,0,0 , 0.3,0.3,0.3 , 0);
+         // cube(1,0,0 , 0.2,0.2,0.2 , 45);
+         // cube(0,1,0 , 0.4,0.4,0.2 , 90);
+         glColor3f(0,1,1);
+         lathe(rocket_profile, ROCKET_POINT_COUNT, 1,1,0, 1,1,0,30, 1.0/25.0);
+         glColor3f(1,1,0);
+         lathe(rocket_profile, ROCKET_POINT_COUNT, -1,0,0, 1,0,1,85, -1.0/20.0);
+         glColor3f(1,0,1);
+         lathe(rocket_profile, ROCKET_POINT_COUNT, 0,0.5,1.5, 0,1,1,161, 1.0/30.0);
+         glColor3f(1,0,0);
+         lathe(rocket_profile, ROCKET_POINT_COUNT, 0,-0.5,-1, 0,1,0,35, -1.0/35.0);
+         glColor3f(0,1,0);
+         lathe(rocket_profile, ROCKET_POINT_COUNT, 1.1,1.1,1.1, 0,0,0,0, 1.0/30.0);
+
+         //  Draw spheres
+         sphere1(0,0,0 , 0.4);
+         sphere1(1,0,0 , 0.2);
+         sphere2(0,1,0 , 0.2);
+         break;
+      //  Line airplane
+      case 2:
+         PolyPlane(GL_LINE_LOOP , 0,0,0);
+         break;
+      //  Polygon airplane
+      case 3:
+         PolyPlane(GL_POLYGON , 0,0,0);
+         break;
+      //  Three flat airplanes
+      case 4:
+         FlatPlane( 0.0, 0.0, 0.0);
+         FlatPlane(-0.5, 0.5,-0.5);
+         FlatPlane(-0.5,-0.5,-0.5);
+         break;
+      // Three solid airplanes
+      case 5:
+         SolidPlane( 0, 0, 0 , 1,0,0 , 0, 1,0);
+         SolidPlane(-1, 1, 0 ,-1,0,0 , 0,-1,0);
+         SolidPlane(-1,-1, 0 ,-1,0,0 , 0, 1,0);
+         break;
+      // Icosahedron
+      case 6:
+         icosahedron(1.2);
+         break;
+      // Mix of objects
+      case 7:
+         //  Cube
+         cube(-1,0,0 , 0.3,0.3,0.3 , 3*zh);
+         //  Ball
+         sphere1(0,0,0 , 0.3);
+         //  Solid Airplane
+         SolidPlane(Cos(zh),Sin(zh), 0 ,-Sin(zh),Cos(zh),0 , Cos(4*zh),0,Sin(4*zh));
+         //  Icosahedron
+         glPushMatrix();
+         glTranslatef(1,0,0);
+         glRotatef(zh,1,0,0);
+         icosahedron(0.5);
+         glPopMatrix();
+         //  Utah Teapot
+         glPushMatrix();
+         glTranslatef(0,0,-1);
+         glRotatef(zh,0,1,0);
+         glColor3f(Cos(zh)*Cos(zh),0,Sin(zh)*Sin(zh));
+         glutSolidTeapot(0.5);
+         glPopMatrix();
+         break;
+   }
+   //  White
+   glColor3f(1,1,1);
+   //  Draw axes
+   if (axes)
+   {
+      glBegin(GL_LINES);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(len,0.0,0.0);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(0.0,len,0.0);
+      glVertex3d(0.0,0.0,0.0);
+      glVertex3d(0.0,0.0,len);
+      glEnd();
+      //  Label axes
+      glRasterPos3d(len,0.0,0.0);
+      Print("X");
+      glRasterPos3d(0.0,len,0.0);
+      Print("Y");
+      glRasterPos3d(0.0,0.0,len);
+      Print("Z");
+   }
+   //  Five pixels from the lower left corner of the window
+   glWindowPos2i(5,25);
+   //  Print the text string
+   Print("Angle=%d,%d",th,ph);
+   //  Render the scene
+   glFlush();
+   //  Make the rendered scene visible
+   glutSwapBuffers();
+}
+
+/*
+ *  GLUT calls this routine when an arrow key is pressed
+ */
+void special(int key,int x,int y)
+{
+   //  Right arrow key - increase angle by 5 degrees
+   if (key == GLUT_KEY_RIGHT)
+      th += 5;
+   //  Left arrow key - decrease angle by 5 degrees
+   else if (key == GLUT_KEY_LEFT)
+      th -= 5;
+   //  Up arrow key - increase elevation by 5 degrees
+   else if (key == GLUT_KEY_UP)
+      ph += 5;
+   //  Down arrow key - decrease elevation by 5 degrees
+   else if (key == GLUT_KEY_DOWN)
+      ph -= 5;
+   //  Keep angles to +/-360 degrees
+   th %= 360;
+   ph %= 360;
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when a key is pressed
+ */
+void key(unsigned char ch,int x,int y)
+{
+   //  Exit on ESC
+   if (ch == 27)
+      exit(0);
+   //  Reset view angle
+   else if (ch == '0')
+      th = ph = 0;
+   //  Toggle axes
+   else if (ch == 'a' || ch == 'A')
+      axes = 1-axes;
+   //  Tell GLUT it is necessary to redisplay the scene
+   glutPostRedisplay();
+}
+
+/*
+ *  GLUT calls this routine when the window is resized
+ */
+void reshape(int width,int height)
+{
+   const double dim=2.5;
+   //  Ratio of the width to the height of the window
+   double w2h = (height>0) ? (double)width/height : 1;
+   //  Set the viewport to the entire window
+   glViewport(0,0, width,height);
+   //  Tell OpenGL we want to manipulate the projection matrix
+   glMatrixMode(GL_PROJECTION);
+   //  Undo previous transformations
+   glLoadIdentity();
+   //  Orthogonal projection
+   glOrtho(-w2h*dim,+w2h*dim, -dim,+dim, -dim,+dim);
+   //  Switch to manipulating the model matrix
+   glMatrixMode(GL_MODELVIEW);
+   //  Undo previous transformations
+   glLoadIdentity();
+}
+
+/*
+ *  GLUT calls this toutine when there is nothing else to do
+ */
+void idle()
+{
+   double t = glutGet(GLUT_ELAPSED_TIME)/1000.0;
+   zh = fmod(90*t,360);
+   glutPostRedisplay();
+}
+
+/*
+ *  Start up GLUT and tell it what to do
+ */
+int main(int argc,char* argv[])
+{
+   //  Initialize GLUT and process user parameters
+   glutInit(&argc,argv);
+   //  Request double buffered, true color window with Z buffering at 600x600
+   glutInitWindowSize(600,600);
+   glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
+   //  Create the window
+   glutCreateWindow("Objects");
+   //  Tell GLUT to call "idle" when there is nothing else to do
+   glutIdleFunc(idle);
+   //  Tell GLUT to call "display" when the scene should be drawn
+   glutDisplayFunc(display);
+   //  Tell GLUT to call "reshape" when the window is resized
+   glutReshapeFunc(reshape);
+   //  Tell GLUT to call "special" when an arrow key is pressed
+   glutSpecialFunc(special);
+   //  Tell GLUT to call "key" when a key is pressed
+   glutKeyboardFunc(key);
+   //  Pass control to GLUT so it can interact with the user
+   glutMainLoop();
+   return 0;
+}
---
+++ makefile
@@ -0,0 +1,46 @@
+# makefile for hw3 - adapted from class example 10 makefile
+EXE=hw3
+
+# Main target
+all: $(EXE)
+
+#  MinGW
+ifeq "$(OS)" "Windows_NT"
+CFLG=-O3 -Wall
+LIBS=-lglut32cu -lglu32 -lopengl32
+CLEAN=del *.exe *.o *.a
+else
+#  OSX
+ifeq "$(shell uname)" "Darwin"
+CFLG=-O3 -Wall -Wno-deprecated-declarations
+LIBS=-framework GLUT -framework OpenGL
+#  Linux/Unix/Solaris
+else
+CFLG=-O3 -Wall
+LIBS=-lglut -lGLU -lGL -lm
+endif
+#  OSX/Linux/Unix/Solaris
+CLEAN=rm -f $(EXE) *.o *.a
+endif
+
+# Dependencies
+hw3.o: hw3.c rocket.h
+
+#  Create archive
+hw3.a:hw3.o
+	ar -rcs $@ $^
+
+# Compile rules
+.c.o:
+	gcc -c $(CFLG) $<
+.cpp.o:
+	g++ -c $(CFLG) $<
+
+#  Link
+hw3:hw3.o hw3.a
+	gcc -O3 -o $@ $^   $(LIBS)
+	./hw3
+
+#  Clean
+clean:
+	$(CLEAN)
